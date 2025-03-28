a5846d9885ae6a5dadf62c12c54f976176febd64
Marcus Castilho <marcus.castilho.castpy@gmail.com>
2025-03-28 15:12:39 -0300

    feat: auth layout

---
+++ src/templates/app/app.template.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React from "react";
+import styles from "./app.module.css";
+import { At, Password } from "@phosphor-icons/react";
+import {
+  Box,
+  Button,
+  Card,
+  Separator,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
+import { AboutPage } from "@/components/about_page/about_page.comp";
+import { SocialAuth } from "@/components/social_auth/social_auth.comp";
+
+const AppTemplate = () => {
+  return (
+    <Box className={styles.main}>
+      <AboutPage
+        position="start"
+        title="Faça login ou cadastre-se"
+        subtitle="Informe seu e-mail e senha ou utilize o google!"
+      />
+
+      <Card className={styles.card}>
+        <form className={styles.form}>
+          <Box className={styles.inputContainer}>
+            <Text className={styles.label} size="3">
+              E-mail
+            </Text>
+            <TextField.Root
+              size="3"
+              type="email"
+              placeholder="E-mail"
+              className={styles.input}
+            >
+              <TextField.Slot>
+                <At size={32} />
+              </TextField.Slot>
+            </TextField.Root>
+          </Box>
+          <Box className={styles.inputContainer}>
+            <Text className={styles.label} size="3">
+              Senha
+            </Text>
+            <TextField.Root
+              size="3"
+              type="password"
+              placeholder="Senha"
+              className={styles.input}
+            >
+              <TextField.Slot>
+                <Password size={32} />
+              </TextField.Slot>
+            </TextField.Root>
+          </Box>
+
+          <Button type="submit" size="3" className={styles.button}>
+            Entrar
+          </Button>
+        </form>
+
+        <Separator size="4" />
+
+        <SocialAuth social="google" />
+      </Card>
+    </Box>
+  );
+};
+
+export default AppTemplate;
