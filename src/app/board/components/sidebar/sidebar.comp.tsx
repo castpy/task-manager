72cc387116a04ef228e531b00da5881784182547
Marcus Castilho <marcus.castilho.castpy@gmail.com>
2025-03-29 14:52:12 -0300

    feat: new routes

--- src/app/board/components/sidebar/sidebar.comp.tsx
+++ src/app/board/components/sidebar/sidebar.comp.tsx
@@ -1,13 +1,7 @@
 "use client";
 import { AppSidebarProps } from "@/components/app_sidebar/types/app_sidebar";
+import { PresentationChart, SignOut, Users } from "@phosphor-icons/react";
 import { AppSidebar } from "@/components/app_sidebar/app_sidebar";
-import {
-  Buildings,
-  House,
-  SignOut,
-  UserCircle,
-  Wallet,
-} from "@phosphor-icons/react";
 import { useApp } from "@/hooks/use.app";
 import React from "react";
 import { useRouter } from "next/navigation";
@@ -17,24 +11,14 @@ export const SideBar = () => {
   const { logOut } = useApp();
   const itens: AppSidebarProps["items"] = [
     {
-      title: "Página Inicial",
-      icon: <House size={22} />,
+      title: "Tarefas",
+      icon: <PresentationChart size={22} />,
       onClick: () => router.push("/"),
     },
     {
-      title: "Meu perfil",
-      icon: <UserCircle size={22} />,
-      onClick: () => router.push("/profile"),
-    },
-    {
-      title: "Minha carteira",
-      icon: <Wallet size={22} />,
-      onClick: () => router.push("/profile/wallet"),
-    },
-    {
-      title: "Empresas/Anunciar Vagas",
-      icon: <Buildings size={22} />,
-      onClick: () => router.push("/business"),
+      title: "Usuários",
+      icon: <Users size={22} />,
+      onClick: () => router.push("/users"),
     },
     {
       title: "Sair",
