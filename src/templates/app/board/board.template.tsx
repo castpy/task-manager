8e2408cc1d7fb5456e48ba566aeaed24c82acb4a
Marcus Castilho <marcus.castilho.castpy@gmail.com>
2025-03-29 15:09:25 -0300

    feat: using header comp

--- src/templates/app/board/board.template.tsx
+++ src/templates/app/board/board.template.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./board.module.css";
 import { Box } from "@radix-ui/themes";
+import HeaderComp from "./components/header/header.comp";
 
 export const BoardTemplate = () => {
   return (
     <Box className={styles.main}>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae et id fuga
-      temporibus corporis? Vel explicabo quibusdam, perspiciatis commodi
-      consectetur nostrum iure autem quae eaque, inventore ipsam reiciendis.
-      Eius, iure.
+      <HeaderComp />
     </Box>
   );
 };
