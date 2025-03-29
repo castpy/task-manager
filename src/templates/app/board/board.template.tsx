8a2a2bbad8fd138868690c10ebcb2dfe2f0c6217
Marcus Castilho <marcus.castilho.castpy@gmail.com>
2025-03-29 14:48:01 -0300

    feat: new board template

---
+++ src/templates/app/board/board.template.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./board.module.css";
+import { Box } from "@radix-ui/themes";
+
+export const BoardTemplate = () => {
+  return (
+    <Box className={styles.main}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae et id fuga
+      temporibus corporis? Vel explicabo quibusdam, perspiciatis commodi
+      consectetur nostrum iure autem quae eaque, inventore ipsam reiciendis.
+      Eius, iure.
+    </Box>
+  );
+};
