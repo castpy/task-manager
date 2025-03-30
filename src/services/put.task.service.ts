6fa3a24e59ebf6cca65da00c204c50f5fe0e760b
Marcus Castilho <marcus.castilho.castpy@gmail.com>
2025-03-30 12:41:32 -0300

    feat: serviço para atualizar o status da tarefa via API

---
+++ src/services/put.task.service.ts
@@ -0,0 +1,22 @@
+import { api } from "@/config/api.axios.config";
+import { AxiosResponse } from "axios";
+
+export const putTask = async (
+  token: string,
+  form: { id: string; status: string }
+): Promise<AxiosResponse["status"]> => {
+  try {
+    const response = await api.post<AxiosResponse["status"]>(
+      "/user/task",
+      form,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.status;
+  } catch (error) {
+    throw error;
+  }
+};
